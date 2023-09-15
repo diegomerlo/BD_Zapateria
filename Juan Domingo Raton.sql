490f67c6e1aad531bfbf1dcf18c482db81b6b417
mikel-ux <85713596+mikel-ux@users.noreply.github.com>
2023-09-15 09:08:55 -0300

    Update Juan Domingo Raton.sql

--- Juan Domingo Raton.sql
+++ Juan Domingo Raton.sql
@@ -3,12 +3,11 @@
 -- CREACIÓN DE LA BASE DE DATOS
 DROP DATABASE IF EXISTS Zapateria;
 CREATE DATABASE Zapateria;
-
 use Zapateria;
-
+-- ------------------------------------------------------------------------------------------
 -- CREACIÓN DE TABLAS
 CREATE TABLE Sucursal (
-codigo_sucursal int NOT NULL ,
+codigo_sucursal int NOT NULL AUTO_INCREMENT,
 Localidad varchar(50),
 Ciudad varchar(50),
 codigo_postal varchar(5),
@@ -19,26 +18,21 @@ PRIMARY KEY (codigo_sucursal)
 );
 
 
-
-
-
-create table Gama (
+CREATE TABLE Gama (
 gama varchar (20) NOT NULL ,
 Descripcion varchar(100) ,
 PRIMARY KEY (gama) 
 );
 
 
-
-create table Area (
+CREATE TABLE Area (
 area char (3) NOT NULL ,
 Descripcion varchar(100) ,
 PRIMARY KEY (Area) 
 );
 
 
-
-create table Producto (
+CREATE TABLE Producto (
 codigo_producto int NOT NULL ,
 nombre varchar(20),
 proveedor varchar(20),
@@ -48,44 +42,33 @@ precio_venta decimal(7,2),
 precio_proveedor decimal(7,2), 
 gama varchar (20) NOT NULL ,
 PRIMARY KEY (codigo_producto),
-FOREIGN KEY (gama) references
-Gama(gama));
+FOREIGN KEY (gama) REFERENCES Gama(gama));
 
 
-
-create table Reparacion (
+CREATE TABLE Reparacion (
 codigo_reparacion int NOT NULL ,
 precio_servicio int,
 descripcion varchar(100),
 area char (3) NOT NULL ,
-	PRIMARY KEY (codigo_reparacion),
-	FOREIGN KEY (area) references
-Area(area));
+PRIMARY KEY (codigo_reparacion),
+FOREIGN KEY (area) REFERENCES Area(area));
 
 
-
-
-create table Estado_Pedido(
+CREATE TABLE Estado_Pedido(
 codigo_estado int NOT NULL ,
 	descripcion varchar(100) not null,
 	PRIMARY KEY (codigo_estado)
 );
 
 
-
-
-create table Medios_de_pago(
+CREATE TABLE Medios_de_pago(
 codigo_medio_de_pago int NOT NULL ,
 	descripcion varchar(100),
 	PRIMARY KEY (codigo_medio_de_pago)
 );
 
 
-
-
-
-
-create table Cliente (
+CREATE TABLE Cliente (
 codigo_cliente int NOT NULL ,
 nombre varchar(20),
 apellido1 varchar(20),
@@ -93,11 +76,10 @@ apellido2 varchar(20),
 email varchar(50) check (email like '%@%'), /*Si tira error aca, ponerlo abajo de todo, junto con las pk y fk o buscar otra syntax aca: https://www.w3schools.com/sql/sql_check.asp*/
 telefono int,
 codigo_sucursal int NOT NULL ,
-	PRIMARY KEY (codigo_cliente),
-	FOREIGN KEY (codigo_sucursal) references
-Sucursal(codigo_sucursal));
+PRIMARY KEY (codigo_cliente),
+FOREIGN KEY (codigo_sucursal) REFERENCES Sucursal(codigo_sucursal));
 
-create table Empleado (
+CREATE TABLE Empleado (
 	codigo_empleado int NOT NULL,
 	nombre varchar(20) not null,
 	apellido1 varchar(20) not null,
@@ -114,16 +96,12 @@ create table Empleado (
 	codigo_jefe int default null,
 	codigo_sucursal int not null ,
 	PRIMARY KEY (codigo_empleado),
-	FOREIGN KEY (codigo_jefe) references
-Empleado(codigo_empleado),
-FOREIGN KEY (codigo_sucursal) references
-Sucursal(codigo_sucursal)
+	FOREIGN KEY (codigo_jefe) REFERENCES Empleado(codigo_empleado),
+	FOREIGN KEY (codigo_sucursal) REFERENCES Sucursal(codigo_sucursal)
 );
 
 
-
-
-create table Pedido (
+CREATE TABLE Pedido (
 	codigo_pedido int not null,
 	fecha_pedido date, 
 	fecha_entregado date default null,
@@ -134,16 +112,14 @@ create table Pedido (
 	codigo_estado int NOT NULL ,
 	codigo_medio_de_pago int NOT NULL ,
 	PRIMARY KEY (codigo_pedido),
-	foreign key (codigo_cliente) references Cliente (codigo_cliente),	
-	foreign key (codigo_empleado) references Empleado (codigo_empleado),	
-	foreign key (codigo_estado) references Estado_Pedido (codigo_estado),	
-	foreign key (codigo_medio_de_pago) references Medios_de_pago (codigo_medio_de_pago));
-	
-	
+	FOREIGN KEY (codigo_cliente) REFERENCES Cliente (codigo_cliente),	
+	FOREIGN KEY (codigo_empleado) REFERENCES Empleado (codigo_empleado),	
+	FOREIGN KEY (codigo_estado) REFERENCES Estado_Pedido (codigo_estado),	
+	FOREIGN KEY (codigo_medio_de_pago) REFERENCES Medios_de_pago (codigo_medio_de_pago)
+);
 
-	
-	
-	create table Detalle_Pedido (
+
+CREATE TABLE Detalle_Pedido (
 	codigo_producto int NOT NULL ,
 	codigo_reparacion int NOT NULL ,
 	codigo_pedido int NOT NULL ,
@@ -152,104 +128,33 @@ create table Pedido (
 	reparacion_precio_servicio decimal(7,2),
 	foreign key (codigo_producto) references Producto (codigo_producto),	
 	foreign key (codigo_reparacion) references Reparacion (codigo_reparacion),	
-	foreign key (codigo_pedido) references Pedido (codigo_pedido));
-	
+	foreign key (codigo_pedido) references Pedido (codigo_pedido)
+);
+
+
+-- ------------------------------------------------------------------------------------------
 -- Inserts
-
-
-
-
-
-insert into Sucursal values (1,"Monte Castro","Ciudad Autonoma de Buenos Aires","C1407",2857463857,2299,"Marcos Paz");
-insert into Sucursal values (2,"Boedo","Ciudad Autonoma de Buenos Aires","C1250 ",1123678954,1101,"Castro");
-insert into Sucursal values (3,"San Cristóbal","Ciudad Autonoma de Buenos Aires","C1253,",9800175643,1099,"Pasco");
-
-
-
-
-
---
-insert into Gama values ("","",);
-
-
-
-
-
-insert into Area values ("","",);
-
-
-
-
-
-insert into Producto values (,"","","",,,,"");
-
-
-
-
-
-
-
-
-insert into Reparacion values (,,"","");
-
-
-
-
-
-
-
-
-
-
-
-
-insert into Estado_Pedido values (,"");
-
-
-
-
-
-
-insert into Medios_de_pago values (,"");
-
-
-
-
-
-
-
-
-
-insert into Cliente values (,"","","","",,);
-
-
-
-
-
-
-
-
-
-
-
-
-
-insert into Empleado values (,"","","",,"","",'','',"",,,"",,);
-
-
-
-
-
-
-
-
-
-insert into Pedido values (,'','',"",'',,,,);
-
-
-
-
-
-
-
-insert into Detalle_Pedido values (,,,,,);
+INSERT INTO Sucursal(Localidad, Ciudad, codigo_postal, telefono, numero_direccion, calle_direccion) VALUES
+						("Monte Castro","Ciudad Autonoma de Buenos Aires","C1407",2857463857,2299,"Marcos Paz"),
+						("Boedo","Ciudad Autonoma de Buenos Aires","C1250 ",1123678954,1101,"Castro"),
+						("San Cristóbal","Ciudad Autonoma de Buenos Aires","C1253,",9800175643,1099,"Pasco");
+
+
+INSERT INTO Gama VALUES ("ID","Infante deportivo"),
+						("NoD","Niño Deportivo"),
+						("NaD","Niña Deportiva"),
+						("AD","Adulto Deportivo"),
+						("HD","Hombre Deportivo"),
+						("MD","Mujer Deportiva"),
+						("ZV","Zapatos de vestir"),
+						("M", "Mocasín"),
+						("B", "Borcegos"),
+						("B.A.", "Botas Australianas");
+                        
+INSERT INTO Area VALUES ("MAR","Marroquineria"),
+						("VAL","Valijero"),
+                        ("ZAP","Zapatero");
+                        
+INSERT INTO Producto(nombre, proveedor, descripcion, cantidad_en_stock, precio_venta, precio_proveedor, gama) VALUES
+						("Air Jordan Low Homage","Nike","Tremendas zapas",40,5000,2000,"AD");
+                        
