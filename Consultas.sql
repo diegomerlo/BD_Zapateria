838f6e2c93e287fc67c872b8964f358190ad6b3a
Diego <109486481+diegomerlo@users.noreply.github.com>
2023-10-02 08:20:51 -0300

    Update Consultas.sql

--- Consultas.sql
+++ Consultas.sql
@@ -1,2 +1,8 @@
-./*----------------Operadores Lógicos-------------*/
-SELECT p.nombre AS Nombre FROM producto p WHERE p.precio_venta BETWEEN 2000 AND 5000; 
+/*------------------Operadores Lógicos------------------*/
+  
+SELECT p.nombre AS Nombre FROM producto p WHERE p.precio_venta BETWEEN 2000 AND 5000;
+
+SELECT c.nombre FROM cliente c WHERE c.nombre LIKE 'F%';
+
+/*------------------Operadores de Fecha------------------*/
+
