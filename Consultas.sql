380dad2eb4845cb43ecc698bc8f12eead57355b4
mikel-ux <85713596+mikel-ux@users.noreply.github.com>
2023-10-02 11:40:06 -0300

    Update Consultas.sql

--- Consultas.sql
+++ Consultas.sql
@@ -1,16 +1,23 @@
 /*------------------Operadores Lógicos------------------*/
-  
+
+#Devuelve el nombre de los productos cuyo precio de venta al público se encuentre entre los $2000 y los $5000
 SELECT p.nombre AS Nombre FROM producto p WHERE p.precio_venta BETWEEN 2000 AND 5000;
 
+#Devuelve el nombre de los clientes cuyo nombre empiece con 'F'
 SELECT c.nombre FROM cliente c WHERE c.nombre LIKE 'F%';
 
 /*------------------Operadores de Fecha------------------*/
 
+#Devuelve las fechas de los pedidos cuya estimación de envío sea enmarcada en el mes de octubre (mes 10)
 SELECT p.fecha_pedido, p.fecha_entregado FROM pedido p WHERE MONTH(p.fecha_envioEstimado) = 10;
 
+#Devuelve las fechas de los pedidos que sí o sí tengan 'fecha de envioEstimado' y que además,
+#cuya diferencia entre la fecha en la que se hizo el pedido y la fecha en la que se estima que el envío llegue, supere los 15 días
 SELECT p.fecha_pedido, p.fecha_entregado, DATEDIFF(p.fecha_envioEstimado,p.fecha_pedido) AS "Dias para la entrega"
 FROM pedido p WHERE p.fecha_envioEstimado IS NOT NULL AND DATEDIFF(p.fecha_envioEstimado,p.fecha_pedido) > 15;
 
+#Devuelve las fechas de los pedidos que sí o sí tengan 'fecha de envíoEstimado' y que además,
+#cuya diferencia entre la fecha en la que se hizo el pedido y la fecha en la que se estima que el envío llegue, sea menor a los 15 días
 SELECT p.fecha_pedido, p.fecha_entregado, DATEDIFF(p.fecha_envioEstimado,p.fecha_pedido) AS "Dias para la entrega"
 FROM pedido p WHERE p.fecha_envioEstimado IS NOT NULL AND DATEDIFF(p.fecha_envioEstimado,p.fecha_pedido) < 15;
 
@@ -32,8 +39,10 @@ HAVING SUM(d.producto_precio_unidad)<25000;
 
 /*------------------Campos calculados (Count, Sum, Max, Min, Avg)------------------*/
 
+#Devuelve la cantidad de clientes que hayan comprado en la sucursal 3
 SELECT COUNT(*) FROM cliente C WHERE C.codigo_sucursal=3;
 
+#Devuelve el promedio total de la plata obtenida tanto en los productos vendidos como en las reparaciones
 SELECT AVG(D.producto_precio_unidad) Producto_avg, AVG(D.reparacion_precio_servicio) Servicio_avg FROM detalle_pedido D;
 
 /*------------------Inner Join------------------*/
@@ -61,6 +70,19 @@ SELECT e.nombre, e.fecha_Alta FROM empleado e ORDER BY e.fecha_Alta ASC;
 
 /*------------------SubConsultas------------------*/
 
+#Devuelve los empleados que hayan vendido pedidos
 SELECT CONCAT(e.apellido1,' ',e.apellido2,', ',e.nombre) AS Nombre,e.puesto AS Puesto 
-  FROM empleado e 
-  WHERE e.codigo_empleado IN (SELECT codigo_empleado FROM pedido);
+FROM empleado e 
+WHERE e.codigo_empleado IN (SELECT codigo_empleado FROM pedido);
+
+
+#Devuelve las sucursales cuyos pedidos facturados contengan servicios de reparación con un costo menor a $100
+SELECT *
+FROM sucursal s
+WHERE s.codigo_sucursal IN (SELECT c.codigo_sucursal 
+                           FROM cliente c
+                           WHERE c.codigo_cliente IN (SELECT p.codigo_cliente
+                                                     FROM pedido p
+                                                     WHERE p.codigo_pedido IN (SELECT d.codigo_pedido
+                                                                              FROM detalle_pedido d
+                                                                              WHERE d.reparacion_precio_servicio<100)));
