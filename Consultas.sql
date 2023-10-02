5f7305c250b7fa87fc9cdf78472cfe92caa16d09
Diego <109486481+diegomerlo@users.noreply.github.com>
2023-10-02 08:27:58 -0300

    Update Consultas.sql

--- Consultas.sql
+++ Consultas.sql
@@ -6,9 +6,12 @@ SELECT c.nombre FROM cliente c WHERE c.nombre LIKE 'F%';
 
 /*------------------Operadores de Fecha------------------*/
 
+SELECT p.fecha_pedido, p.fecha_entregado FROM pedido p WHERE MONTH(p.fecha_envioEstimado) = 10;
+
 
 
 /*------------------Campos calculados (Count, Sum, Max, Min, Avg)--------------------------------------*/
+
 SELECT COUNT(*) FROM cliente C WHERE C.codigo_sucursal=3;
 
 SELECT AVG(D.producto_precio_unidad) Producto_avg, AVG(D.reparacion_precio_servicio) Servicio_avg FROM detalle_pedido D;
