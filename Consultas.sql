70f616b94dadfeb61f14d2ea1864eb0f1844dcee
Diego <109486481+diegomerlo@users.noreply.github.com>
2023-10-02 08:55:56 -0300

    Update Consultas.sql

--- Consultas.sql
+++ Consultas.sql
@@ -14,7 +14,7 @@ FROM pedido p WHERE p.fecha_envioEstimado IS NOT NULL AND DATEDIFF(p.fecha_envio
 SELECT p.fecha_pedido, p.fecha_entregado, DATEDIFF(p.fecha_envioEstimado,p.fecha_pedido) AS "Dias para la entrega"
 FROM pedido p WHERE p.fecha_envioEstimado IS NOT NULL AND DATEDIFF(p.fecha_envioEstimado,p.fecha_pedido) < 15;
 
-/*-------------------Agrupación-------------------------------*/
+/*------------------Agrupación------------------*/
 #Devuelve la cantidad de veces de los métodos de pago que hayan sido usados menos de 5 veces
 SELECT COUNT(*) 'cantidad de veces', p.codigo_medio_de_pago
 FROM pedido p
@@ -23,13 +23,13 @@ HAVING COUNT(p.codigo_medio_de_pago) < 5;
 
 
 
-/*------------------Campos calculados (Count, Sum, Max, Min, Avg)--------------------------------------*/
+/*------------------Campos calculados (Count, Sum, Max, Min, Avg)------------------*/
 
 SELECT COUNT(*) FROM cliente C WHERE C.codigo_sucursal=3;
 
 SELECT AVG(D.producto_precio_unidad) Producto_avg, AVG(D.reparacion_precio_servicio) Servicio_avg FROM detalle_pedido D;
 
-/*------------------Inner Join-------------------------*/
+/*------------------Inner Join------------------*/
 
 #Devuelve los clientes que hayan comprado un producto clasificado en la gama 'M' (mocasín)
 SELECT c.* FROM cliente c
@@ -46,3 +46,6 @@ INNER JOIN pedido p ON c.codigo_cliente=p.codigo_cliente
 INNER JOIN medios_de_pago m ON p.codigo_medio_de_pago=m.codigo_medio_de_pago
 WHERE m.codigo_medio_de_pago='MEP';
 
+/*------------------Order By------------------*/
+
+SELECT dp.codigo_producto AS Codigo,dp.producto_precio_unidad AS Precio FROM detalle_pedido dp ORDER BY dp.producto_precio_unidad ASC;
