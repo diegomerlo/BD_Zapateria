e45c82a5437af05de128e4d229d16c67227a6aa3
mikel-ux <85713596+mikel-ux@users.noreply.github.com>
2023-10-02 08:26:43 -0300

    Update Consultas.sql

--- Consultas.sql
+++ Consultas.sql
@@ -6,3 +6,9 @@ SELECT c.nombre FROM cliente c WHERE c.nombre LIKE 'F%';
 
 /*------------------Operadores de Fecha------------------*/
 
+
+
+/*------------------Campos calculados (Count, Sum, Max, Min, Avg)--------------------------------------*/
+SELECT COUNT(*) FROM cliente C WHERE C.codigo_sucursal=3;
+
+SELECT AVG(D.producto_precio_unidad) Producto_avg, AVG(D.reparacion_precio_servicio) Servicio_avg FROM detalle_pedido D;
