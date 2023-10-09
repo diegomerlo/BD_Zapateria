af78e857b985573d502e4329c28c927abda07552
mikel-ux <85713596+mikel-ux@users.noreply.github.com>
2023-10-09 11:34:58 -0300

    Update Procedimientos_y_Funciones.sql

--- Procedimientos_y_Funciones.sql
+++ Procedimientos_y_Funciones.sql
@@ -8,4 +8,21 @@ BEGIN
             WHERE r.codigo_reparacion=id);
 END//
 DELIMITER ;
+#_________________________________________________________________________________________________________________________________
+DELIMITER //
+CREATE OR REPLACE FUNCTION descuento(id INT, porcentaje INT)
+RETURNS INT
+BEGIN
+	DECLARE descuento INT;
+    DECLARE precio INT;
+    SET precio = (SELECT precio_servicio 
+            	  FROM reparacion r
+            	  WHERE r.codigo_reparacion=id);
+	SET descuento = (porcentaje/100) * precio;
+    
+	RETURN (precio - descuento);
+END//
+DELIMITER ;
 
+SELECT r.*, descuento(r.codigo_reparacion,50) FROM reparacion r;
+#_________________________________________________________________________________________________________________________________
