fe288ab4c0e725717552cb3f0fcedb33b5a0201c
mikel-ux <85713596+mikel-ux@users.noreply.github.com>
2023-10-09 11:24:32 -0300

    Update Procedimientos_y_Funciones.sql

--- Procedimientos_y_Funciones.sql
+++ Procedimientos_y_Funciones.sql
@@ -1,1 +1,11 @@
+#Función que devuelve el precio de la reparación indicando su codigo id;
+DELIMITER //
+CREATE OR REPLACE FUNCTION precio(id INT)
+RETURNS INT
+BEGIN
+	RETURN (SELECT precio_servicio 
+            FROM reparacion r
+            WHERE r.codigo_reparacion=id);
+END//
+DELIMITER ;
 
