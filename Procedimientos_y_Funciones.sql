4d0687f9741114bcac0d32fce57bd21e4a0a1aff
mikel-ux <85713596+mikel-ux@users.noreply.github.com>
2023-10-30 09:01:47 -0300

    Update Procedimientos_y_Funciones.sql

--- Procedimientos_y_Funciones.sql
+++ Procedimientos_y_Funciones.sql
@@ -26,3 +26,27 @@ DELIMITER ;
 
 SELECT r.*, descuento(r.codigo_reparacion,50) FROM reparacion r;
 #_________________________________________________________________________________________________________________________________
+#Procedimiento que cambia el stock de un producto especifico 
+	#(HAY QUE SOLUCIONAR UN ERROR)
+DROP PROCEDURE IF EXISTS modificarStock;
+DELIMITER //
+CREATE OR REPLACE PROCEDURE modificarStock(idProducto INT, cantidadCambio INT, accion VARCHAR(50))
+BEGIN
+	DECLARE oldStock INT;
+    DECLARE newStock INT;
+    
+    SET oldStock=(SELECT cantidad_en_stock FROM producto WHERE codigo_producto=idProducto);
+    
+    IF accion IS IN("mas", "+", "sumar", "aumentar", "llenar", "rellenar", "poner") THEN
+    	SET newStock = oldStock + cantidadCambio;
+	ELSEIF accion IS IN("menos", "-", "restar", "disminuir", "vaciar", "sacar") THEN         
+		SET newStock = oldStock - cantidadCambio;
+	END IF;
+    
+    SELECT newStock;
+END//
+DELIMITER ;
+
+CALL modificarStock(1, 10, "mas");
+
+
