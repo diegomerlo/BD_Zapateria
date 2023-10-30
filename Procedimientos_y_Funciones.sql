4d1fc2b88089cbab40568f05d820fcfab9884126
mikel-ux <85713596+mikel-ux@users.noreply.github.com>
2023-10-30 11:16:23 -0300

    Update Procedimientos_y_Funciones.sql

--- Procedimientos_y_Funciones.sql
+++ Procedimientos_y_Funciones.sql
@@ -27,7 +27,9 @@ DELIMITER ;
 SELECT r.*, descuento(r.codigo_reparacion,50) FROM reparacion r;
 #_________________________________________________________________________________________________________________________________
 #Procedimiento que cambia el stock de un producto especifico 
-	#(HAY QUE SOLUCIONAR UN ERROR)
+	#Se indica el ID del producto y la cantidad que se sumará o restará al stock.
+	#Mediante el 3er parámetro se indica si se está llenando o vaciando el stock, así es reutilizable el PROCEDURE.
+	#En caso de que se quiera restar una cantidad mayor de la cual hay en el stock, se omite la operación y se mantiene el antiguo stock
 DROP PROCEDURE IF EXISTS modificarStock;
 DELIMITER //
 CREATE OR REPLACE PROCEDURE modificarStock(idProducto INT, cantidadCambio INT, accion VARCHAR(50))
@@ -37,16 +39,22 @@ BEGIN
     
     SET oldStock=(SELECT cantidad_en_stock FROM producto WHERE codigo_producto=idProducto);
     
-    IF accion IS IN("mas", "+", "sumar", "aumentar", "llenar", "rellenar", "poner") THEN
+    IF accion IN("mas", "+", "sumar", "aumentar", "llenar", "rellenar", "poner") THEN
     	SET newStock = oldStock + cantidadCambio;
-	ELSEIF accion IS IN("menos", "-", "restar", "disminuir", "vaciar", "sacar") THEN         
-		SET newStock = oldStock - cantidadCambio;
+	ELSEIF accion IN("menos", "-", "restar", "disminuir", "vaciar", "sacar") THEN
+    	IF oldStock>=cantidadCambio THEN
+			SET newStock = oldStock - cantidadCambio;
+        ELSE
+        	SET newStock = oldStock;
+        END IF;    
 	END IF;
     
-    SELECT newStock;
+    
+    UPDATE producto p SET p.cantidad_en_stock = newStock WHERE codigo_producto=idProducto;
+    SELECT * FROM producto;
 END//
 DELIMITER ;
 
-CALL modificarStock(1, 10, "mas");
-
+CALL modificarStock(2, 465, "mas");
+#_________________________________________________________________________________________________________________________________
 
