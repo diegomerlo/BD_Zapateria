3b1667848400ff230ff8ce9355ec274ecacee1b4
AgusSCode <138786029+AgusSCode@users.noreply.github.com>
2023-11-12 15:24:13 -0300

    Create Triggers.sql

---
+++ Triggers.sql
@@ -0,0 +1,12 @@
+DELIMITER //
+CREATE TRIGGER ValidacionPreviaGama
+BEFORE INSERT ON Gama
+FOR EACH ROW
+BEGIN
+      IF NEW.gama IS NULL THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'La columna requerida no puede ser nula';
+    END IF;
+END;
+//
+DELIMITER ;
