27121ed3c5396ef9e1b252ef2ad0ccc452e752ba
Diego <109486481+diegomerlo@users.noreply.github.com>
2023-11-13 08:49:34 -0300

    Update usuarios.sql

--- usuarios.sql
+++ usuarios.sql
@@ -4,7 +4,7 @@ GRANT SELECT, INSERT, UPDATE, DELETE ON zapateria.sucursal TO 'Gerente'@'localho
 
 #Crear usuario Contador, que puede realizar modificaciones solamente en la vista de determinador pagos hechos con mercado pago
 CREATE USER IF NOT EXISTS 'Contador'@'localhost' IDENTIFIED BY '0000';
-GRANT SELECT, INSERT, UPDATE, DELETE ON zapateria.mercado_pago TO 'Contador'@'localhost';
+GRANT SELECT, UPDATE ON zapateria.mercado_pago TO 'Contador'@'localhost';
 
 #Crear usuario Jefe, que puede revisar la vista que solamente tiene a los empleados que no han sido dados de baja (que no han sido echados)
 CREATE USER IF NOT EXISTS 'Jefe'@'localhost' IDENTIFIED BY 'JEFE';
