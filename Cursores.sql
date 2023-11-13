59993f61ec75cda06298bd682a0dccab1e6f385e
Diego <109486481+diegomerlo@users.noreply.github.com>
2023-11-13 08:08:13 -0300

    Cursores.sql

--- Cursores.sql
+++ Cursores.sql
@@ -29,3 +29,45 @@ BEGIN
 END//
 
 DELIMITER ;
+
+
+
+drop procedure if exists buscar_valores;
+DELIMITER //
+
+CREATE PROCEDURE buscar_valores(IN valor INTEGER)
+BEGIN
+
+		CREATE TEMPORARY TABLE valores (
+        columna1 tipo_dato1,
+        columna2 tipo_dato2
+        -- Puedes agregar más columnas según sea necesario
+    );
+
+    DECLARE suma INTEGER;
+    DECLARE precios INTEGER;
+    DECLARE terminar INT DEFAULT 0;
+
+    DECLARE c CURSOR FOR SELECT precio_venta FROM Producto;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET terminar = 1;
+
+    SET suma = 0;
+
+    OPEN c;
+
+    read_loop: LOOP
+        FETCH c INTO precios;
+        IF terminar = 1 THEN
+            LEAVE read_loop;
+        END IF;
+
+        SET suma = suma + precios;
+    END LOOP;
+
+    CLOSE c;
+
+    SET preciototal = suma;
+END//
+
+DELIMITER ;
+
