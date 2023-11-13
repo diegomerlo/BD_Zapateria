ce929b9f52fe0153118fa66a6139763be8106e79
Diego <109486481+diegomerlo@users.noreply.github.com>
2023-11-13 08:38:20 -0300

    Update Cursores.sql

--- Cursores.sql
+++ Cursores.sql
@@ -32,41 +32,48 @@ DELIMITER ;
 
 
 
+
+
 drop procedure if exists buscar_valores;
 DELIMITER //
 
 CREATE PROCEDURE buscar_valores(IN valor INTEGER)
-BEGIN
+    
+    BEGIN
+		DECLARE precios INTEGER;
+        DECLARE terminar INT DEFAULT 0;
 
-CREATE TEMPORARY TABLE valores (
-        valor_aux int
-    );
-
-    DECLARE precios INTEGER;
-    DECLARE terminar INT DEFAULT 0;
-
-    DECLARE c CURSOR FOR SELECT reparacion_precio_servicio FROM detalle_pedido WHERE reparacion_precio_servicio > valor;
-    DECLARE CONTINUE HANDLER FOR NOT FOUND SET terminar = 1;
+        DECLARE c CURSOR FOR SELECT reparacion_precio_servicio FROM detalle_pedido WHERE reparacion_precio_servicio > valor;
+        DECLARE CONTINUE HANDLER FOR NOT FOUND SET terminar = 1;
+    
+        CREATE TEMPORARY TABLE valores (
+                valor_aux int
+            );
 
 
-    OPEN c;
 
-    read_loop: LOOP
-        FETCH c INTO precios;
-	INSERT INTO valores (valor_aux) VALUES (precios);
-        IF terminar = 1 THEN
-            LEAVE read_loop;
-        END IF;
 
-        SET suma = suma + precios;
-    END LOOP;
+            OPEN c;
 
-    CLOSE c;
+            read_loop: LOOP
+                FETCH c INTO precios;
+            INSERT INTO valores (valor_aux) VALUES (precios);
+                IF terminar = 1 THEN
+                    LEAVE read_loop;
+                END IF;
+            END LOOP;
 
-	SELECT * FROM valores;
-DROP TEMPORARY TABLE valores;
+            CLOSE c;
 
-END//
+            SELECT valor_aux AS "Precios de Venta" FROM valores;
+        	
+            DROP TEMPORARY TABLE valores;
+
+    END//
 
 DELIMITER ;
+
+CALL buscar_valores(160);
+
+
 
