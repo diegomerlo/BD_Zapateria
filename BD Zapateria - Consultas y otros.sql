8c78b893e78c1d449f387859ed15e3e5fc0d55bf
mikel-ux <85713596+mikel-ux@users.noreply.github.com>
2023-11-22 10:33:01 -0300

    Update BD Zapateria - Consultas y otros.sql

--- BD Zapateria - Consultas y otros.sql
+++ BD Zapateria - Consultas y otros.sql
@@ -156,6 +156,41 @@ DELIMITER ;
 
 /*-----------------------------------------------------CURSORES------------------------------------------------------------------------------------------------------*/
 
+
+
+
+/*-----------------------------------------------PROCEDURE y FUNCTION------------------------------------------*/
+#Función que devuelve el precio de la reparación indicando su codigo id;
+DELIMITER //
+CREATE OR REPLACE FUNCTION precio(id INT)
+RETURNS INT
+BEGIN
+	RETURN (SELECT precio_servicio 
+            FROM reparacion r
+            WHERE r.codigo_reparacion=id);
+END//
+DELIMITER ;
+#_________________________________________________________________________________________________________________________________
+#Funcion que devuelve el precio de un producto con un porcentaje de descuento aplicado
+DELIMITER //
+CREATE OR REPLACE FUNCTION descuento(id INT, porcentaje INT)
+RETURNS INT
+BEGIN
+	DECLARE descuento INT;
+    DECLARE precio INT;
+    SET precio = (SELECT precio_servicio 
+            	  FROM reparacion r
+            	  WHERE r.codigo_reparacion=id);
+	SET descuento = (porcentaje/100) * precio;
+    
+	RETURN (precio - descuento);
+END//
+DELIMITER ;
+
+SELECT r.*, descuento(r.codigo_reparacion,50) FROM reparacion r;
+#_________________________________________________________________________________________________________________________________
+
+
 -- Suma el precio de todos los productos y los muestra, el handler se encarga de tratar el error al finalizar la lectura del cursor a traves de un booleano
 drop procedure if exists suma_productos;
 DELIMITER //
