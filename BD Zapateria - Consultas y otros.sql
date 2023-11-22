03cc74ea522581d6644591b3841b9c67ded7dce5
Diego <109486481+diegomerlo@users.noreply.github.com>
2023-11-22 11:55:57 -0300

    Update BD Zapateria - Consultas y otros.sql

--- BD Zapateria - Consultas y otros.sql
+++ BD Zapateria - Consultas y otros.sql
@@ -197,29 +197,30 @@ DELIMITER //
 
 CREATE PROCEDURE suma_productos(OUT preciototal INTEGER)
 BEGIN
-    DECLARE suma INTEGER;
-    DECLARE precios INTEGER;
-    DECLARE terminar INT DEFAULT 0;
+  DECLARE suma INTEGER;
+  DECLARE precios INTEGER;
+  DECLARE terminar INT DEFAULT 0;
 
-    DECLARE c CURSOR FOR SELECT precio_venta FROM Producto;
-    DECLARE CONTINUE HANDLER FOR NOT FOUND SET terminar = 1;
+  DECLARE c CURSOR FOR SELECT precio_venta FROM Producto;
+  DECLARE CONTINUE HANDLER FOR NOT FOUND SET terminar = 1;
 
-    SET suma = 0;
+  SET suma = 0;
 
-    OPEN c;
+  OPEN c;
 
-    read_loop: LOOP
-        FETCH c INTO precios;
-        IF terminar = 1 THEN
-            LEAVE read_loop;
-        END IF;
+  read_loop: LOOP
+    FETCH c INTO precios;
+    IF terminar = 1 THEN
+      LEAVE read_loop;
+    END IF;
 
-        SET suma = suma + precios;
-    END LOOP;
+    SET suma = suma + precios;
+  END LOOP;
 
-    CLOSE c;
+  CLOSE c;
 
-    SET preciototal = suma;
+  SET preciototal = suma;
+
 END//
 
 DELIMITER ;
